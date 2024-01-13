273db57775fb1c8d562ab26fadfd058117005151
Matteo <matteo.jadotdufour@gmail.com>
2024-01-13 14:51:32 +0100

    Optimisation, correction des erreurs de documentation, et ajouts de commentaires utiles pour le fichier some_tests.py

--- some_tests.py
+++ some_tests.py
@@ -15,12 +15,17 @@ print("\x1B[43m \x1B[0m : carré jaune ")
 print("\x1B[41m \x1B[0m : carré rouge ")
 print("\x1B[41mA\x1B[0m : A sur fond rouge")
 
+# Construction d'un plateau test composé de 30 pions de couleurs aléatoires
 p = construirePlateau()
 for _ in range(30):
     placerPionPlateau(p, construirePion(choice(const.COULEURS)), randint(0, const.NB_COLUMNS - 1))
+
+# Conversion du plateau en chaîne de caractères afin de pouvoir tester les fonctions visuellement
 print(toStringPlateau(p))
+# Tests des différentes fonctions de détection de 4 pions alignés pour une couleur définie
 print(detecter4horizontalPlateau(p, const.ROUGE))
 print(detecter4verticalPlateau(p, const.ROUGE))
 print(detecter4diagonaleDirectePlateau(p, const.ROUGE))
 print(detecter4diagonaleIndirectePlateau(p, const.ROUGE))
+# Test de la fonction permettant d'obtenir tous les pions alignés du plateau, peu importe la couleur
 print(getPionsGagnantsPlateau(p))
